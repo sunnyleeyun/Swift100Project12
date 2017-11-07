5e78275c36cc36a1d151d6a5a247c2713998b7ed
Sunny <sunnylee945@hotmail.com>
2017-11-07 16:46:30 +0800

    add model and extension to download image async

---
+++ project12/Brain.swift
@@ -0,0 +1,13 @@
+//
+//  Brain.swift
+//  project12
+//
+//  Created by Mac on 2017/11/7.
+//  Copyright © 2017年 sunnyleeyun. All rights reserved.
+//
+
+import Foundation
+class Brain: NSObject{
+  var imageUrl: String? // https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg
+  var title: String?
+}
