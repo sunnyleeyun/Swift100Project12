ee155a03786e18e94ba5d270f52d19bb8a7ad3af
Sunny <sunnylee945@hotmail.com>
2017-11-07 14:46:21 +0800

    pod firebase and add collectionview in main storyboard

---
+++ Pods/Target Support Files/Pods-project12/Pods-project12-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_project12 : NSObject
+@end
+@implementation PodsDummy_Pods_project12
+@end
---
+++ project12/ImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.swift
+//  project12
+//
+//  Created by Mac on 2017/11/7.
+//  Copyright © 2017年 sunnyleeyun. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+  @IBOutlet weak var imgImage: UIImageView!
+  
+}
